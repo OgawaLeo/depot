4c491fe1b74ddcaf5112cdcb49836123031fc0be
Leo <liuhenan1989@gmail.com>
2016-09-07 19:52:47 +0900

    Validation!

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,46 +3,74 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+        title:       'Lorem Ipsum',
+        description: 'Wibbles are fun!',
+        image_url:   'lorem.jpg',
+        price:       19.95
+    }
   end
 
   test "should get index" do
+    # Attention, syntax like 'get :index' has become invalid for rails 5; use 'get products_url' instead.
+    # get :index
     get products_url
     assert_response :success
+
+    # assigns has been extracted to a gem: gem 'rails-controller-testing'
+    # assert_not_nil assigns(:products)
   end
 
   test "should get new" do
+    # get :new
     get new_product_url
     assert_response :success
   end
 
   test "should create product" do
+    # 1 by default
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      # post :create, product: @update
+      post products_url, params: { product: @update }
+      # post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
     end
 
+    # Attention: product_path and product_url??
+    # assert_redirected_to product_path(assigns(:product))
     assert_redirected_to product_url(Product.last)
   end
 
+  # ...
+
   test "should show product" do
+    # get :show, id: @product
     get product_url(@product)
     assert_response :success
   end
 
   test "should get edit" do
+    # get :edit, id: @product
     get edit_product_url(@product)
     assert_response :success
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    # patch :update, id: @product, product: @update
+    # assert_redirected_to product_path(assigns(:product))
+
+    patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
   end
 
+  # ...
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
+      # delete :destroy, id: @product
       delete product_url(@product)
     end
 
-    assert_redirected_to products_url
+    assert_redirected_to products_path
   end
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @test = Product.new(
+        title:       'Lorem Ipsum',
+        description: 'Wibbles are fun!',
+        image_url:   'lorem.jpg',
+        price:       19.95
+    )
+  end
+
+  test "product attributes test" do
+    @product = Product.new
+    assert @product.invalid?
+    title
+    description
+    price
+    image_url
+  end
+
+  test 'product price must be postive and greater than 0.01' do
+    @test.price = -1
+    assert @test.invalid?
+    # assert_equal [I18n.translate('errors.messages.greater_than_or_equal_to')], @test.errors[:price]
+
+    @test.price = 0
+    assert @test.invalid?
+    # assert_equal [I18n.translate('errors.messages.greater_than_or_equal_to')], @test.errors[:price]
+
+    @test.price = 1
+    assert @test.valid?
+  end
+
+  test 'image url' do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.JPG
+         http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc gred.gif/more fred.gif.more }
+    ok.each do |name|
+      @test.image_url = name
+      assert @test.valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      @test.image_url = name
+      assert @test.invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test 'product is not valid without a unique title' do
+    @test.title = products(:one).title
+    assert @test.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], @test.errors[:title]
+  end
+
+  def method_missing(name)
+    @product.errors[name].any?
+  end
 end
